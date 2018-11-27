8e3a0044c67a5916b5ddb26c58573c2d990e02ee
huaqiang.wu <huaqiang.wu@m2micro.com>
2018-11-27 10:29:54 +0800

    uuid

--- src/main/java/com/m2micro/m2mfa/common/util/UUIDUtil.java
+++ src/main/java/com/m2micro/m2mfa/common/util/UUIDUtil.java
@@ -10,6 +10,6 @@ import java.util.UUID;
 public class UUIDUtil {
 
     public static String getUUID(){
-        return UUID.randomUUID().toString().replace("-", "");
+        return UUID.randomUUID().toString().replace("-", "").toUpperCase();
     }
 }
