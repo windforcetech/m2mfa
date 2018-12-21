7741886d7cdc65643da412a2693da7d1dda71ebd
chenshuhong <shuhong.chen@m2micro.com>
2018-12-21 17:14:31 +0800

    工艺基本流程

--- src/main/java/com/m2micro/m2mfa/mo/model/PartsRouteModel.java
+++ src/main/java/com/m2micro/m2mfa/mo/model/PartsRouteModel.java
@@ -1,28 +1,29 @@
 package com.m2micro.m2mfa.mo.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class PartsRouteModel {
-
+    @ApiModelProperty(value = "料件ID")
     private String partId;
-
+    @ApiModelProperty(value = "料件编号")
     private String partNo;
-
+    @ApiModelProperty(value = "品名")
     private String name;
-
+    @ApiModelProperty(value = "规格")
     private String spec;
-
+    @ApiModelProperty(value = "工艺主表ID")
     private String  routeId;
-
+    @ApiModelProperty(value = "工艺名称")
     private String routeName;
-
+    @ApiModelProperty(value = "投入工序ID")
     private String inputProcessId;
-
+    @ApiModelProperty(value = "投入工序名称")
     private String inputProcessName;
-
+    @ApiModelProperty(value = "产出工序ID")
     private String outputProcessId;
-
+    @ApiModelProperty(value = "产出工序名称")
     private String outputProcessName;
 
 }
