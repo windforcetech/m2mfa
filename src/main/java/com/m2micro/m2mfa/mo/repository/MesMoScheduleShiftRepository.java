1cfae5402b135c2481121adb004024c01bb28d3f
chenshuhong <shuhong.chen@m2micro.com>
2019-01-04 16:03:53 +0800

    排产单获取机台信息

---
+++ src/main/java/com/m2micro/m2mfa/mo/repository/MesMoScheduleShiftRepository.java
@@ -0,0 +1,14 @@
+package com.m2micro.m2mfa.mo.repository;
+
+import com.m2micro.m2mfa.mo.entity.MesMoScheduleShift;
+import com.m2micro.framework.commons.BaseRepository;
+import org.springframework.stereotype.Repository;
+/**
+ *  Repository 接口
+ * @author liaotao
+ * @since 2019-01-04
+ */
+@Repository
+public interface MesMoScheduleShiftRepository extends BaseRepository<MesMoScheduleShift,String> {
+
+}
