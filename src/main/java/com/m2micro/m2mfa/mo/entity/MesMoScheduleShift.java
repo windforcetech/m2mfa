4cbd2c61622e54e1c0dbc98a9759e3d265f63e7c
chenshuhong <shuhong.chen@m2micro.com>
2019-01-04 16:02:25 +0800

    排产单获取机台信息

---
+++ src/main/java/com/m2micro/m2mfa/mo/entity/MesMoScheduleShift.java
@@ -0,0 +1,52 @@
+package com.m2micro.m2mfa.mo.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+import com.m2micro.m2mfa.common.entity.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * 
+ * @author liaotao
+ * @since 2019-01-04
+ */
+@Entity
+@ApiModel(value="MesMoScheduleShift对象", description="")
+public class MesMoScheduleShift extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String id;
+    @ApiModelProperty(value = "排产单Id")
+    private String scheduleId;
+    @ApiModelProperty(value = "班别ID")
+    private String shiftId;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getScheduleId() {
+        return scheduleId;
+    }
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public String getShiftId() {
+        return shiftId;
+    }
+    public void setShiftId(String shiftId) {
+        this.shiftId = shiftId;
+    }
+
+
+
+}
