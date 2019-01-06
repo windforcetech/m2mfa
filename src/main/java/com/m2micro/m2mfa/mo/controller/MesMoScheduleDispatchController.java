609ab02fcc8a8f7179d575cf1a71e59fa65ecdfd
tao.liao <tao.liao@m2micro.com>
2019-01-06 11:42:44 +0800

    增加排产单调度

--- src/main/java/com/m2micro/m2mfa/mo/controller/MesMoScheduleDispatchController.java
+++ src/main/java/com/m2micro/m2mfa/mo/controller/MesMoScheduleDispatchController.java
@@ -4,12 +4,18 @@ import com.m2micro.framework.authorization.Authorize;
 import com.m2micro.framework.commons.annotation.UserOperationLog;
 import com.m2micro.framework.commons.model.ResponseMessage;
 import com.m2micro.m2mfa.base.node.TreeNode;
+import com.m2micro.m2mfa.mo.model.MesMoScheduleModel;
+import com.m2micro.m2mfa.mo.model.ScheduleSequenceModel;
 import com.m2micro.m2mfa.mo.service.MesMoScheduleDispatchService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Auther: liaotao
@@ -32,5 +38,26 @@ public class MesMoScheduleDispatchController {
     @UserOperationLog("获取所有已经绑定的机台及部门")
     public ResponseMessage<TreeNode> getAllDepartAndMachine(){
         return ResponseMessage.ok(mesMoScheduleDispatchService.getAllDepartAndMachine());
+    }
+
+    /**
+     * 获取调度页面的排产单
+     */
+    @RequestMapping("/getScheduleDispatch")
+    @ApiOperation(value="获取调度页面的排产单")
+    @UserOperationLog("获取调度页面的排产单")
+    public ResponseMessage<List<MesMoScheduleModel>> getScheduleDispatch(String machineId){
+        return ResponseMessage.ok(mesMoScheduleDispatchService.getScheduleDispatch(machineId));
+    }
+
+    /**
+     * 更新排产单顺序
+     */
+    @RequestMapping("/updateSequence")
+    @ApiOperation(value="更新排产单顺序")
+    @UserOperationLog("更新排产单顺序")
+    public ResponseMessage updateSequence(@RequestBody ScheduleSequenceModel[] scheduleSequenceModels){
+        mesMoScheduleDispatchService.updateSequence(scheduleSequenceModels);
+        return ResponseMessage.ok();
     }
 }
