e893b07894d986fe454d082df63da4b47101f063
chenshuhong <shuhong.chen@m2micro.com>
2019-01-13 16:00:33 +0800

    排产单缺勤

--- src/main/java/com/m2micro/m2mfa/mo/service/impl/MesMoScheduleAbsenceServiceImpl.java
+++ src/main/java/com/m2micro/m2mfa/mo/service/impl/MesMoScheduleAbsenceServiceImpl.java
@@ -2,6 +2,8 @@ package com.m2micro.m2mfa.mo.service.impl;
 
 import com.m2micro.framework.commons.exception.MMException;
 import com.m2micro.m2mfa.common.util.UUIDUtil;
+import com.m2micro.m2mfa.iot.entity.IotMachineOutput;
+import com.m2micro.m2mfa.iot.service.IotMachineOutputService;
 import com.m2micro.m2mfa.mo.constant.MoStatus;
 import com.m2micro.m2mfa.mo.entity.MesMoSchedule;
 import com.m2micro.m2mfa.mo.entity.MesMoScheduleStaff;
@@ -38,6 +40,8 @@ public class MesMoScheduleAbsenceServiceImpl implements MesMoScheduleAbsenceServ
     MesMoScheduleStaffRepository mesMoScheduleStaffRepository;
     @Autowired
     MesMoScheduleStaffService mesMoScheduleStaffService;
+    @Autowired
+    private IotMachineOutputService iotMachineOutputService;
     @Override
     public List<Absence> mesMoScheduleAbsence(String staffId) {
         String sql ="SELECT bm. CODE machineCode, mms.schedule_no scheduleNo,mms.schedule_id scheduleId, bp.part_no partNo, bps.process_name processName, bsson. NAME stationName, mmss.create_on createTime, bsft. NAME shiftName FROM mes_mo_schedule_staff mmss LEFT JOIN mes_mo_schedule mms ON mmss.schedule_id = mms.schedule_id LEFT JOIN base_staff bs ON bs.staff_id = mmss.staff_id LEFT JOIN base_machine bm ON bm.machine_id = mms.machine_id LEFT JOIN organization o ON bm.department_id = o.uuid LEFT JOIN mes_mo_desc mmd ON mmd.mo_id = mms.mo_id LEFT JOIN base_parts bp ON bp.part_id = mmd.part_id LEFT JOIN base_process bps ON bps.process_id = mmss.process_id LEFT JOIN base_station bsson ON bsson.station_id = mmss.station_id LEFT JOIN base_shift bsft ON bsft.shift_id = mmss.shift_id WHERE mmss.staff_id = '"+staffId+"' AND mmss.actual_start_time IS NULL OR ( mmss.actual_start_time IS NOT NULL AND mmss.actual_end_time IS NULL ) GROUP BY mms.schedule_id AND mms.flag = "+ MoStatus.AUDITED.getKey()+" OR mms.flag = "+MoStatus.SCHEDULED.getKey()+"";
@@ -50,7 +54,13 @@ public class MesMoScheduleAbsenceServiceImpl implements MesMoScheduleAbsenceServ
                 List<MesRecordStaff>mesRecordStaffs =  mesRecordStaffRepository.findStaffId(absencePersonnel.getLackstaffId());
                 if(!mesRecordStaffs.isEmpty()){
                     for(MesRecordStaff mesRecordStaff:mesRecordStaffs){
+                        MesMoSchedule mesMoSchedule =  mesMoScheduleRepository.findById(mesRecordStaff.getScheduleId()).orElse(null);
+                        IotMachineOutput iotMachineOutput = iotMachineOutputService.findIotMachineOutputByMachineId(mesMoSchedule.getMachineId());
                         mesRecordStaff.setEndTime(new Date());
+                        if(iotMachineOutput !=null){
+                            mesRecordStaff.setEndMolds(iotMachineOutput.getOutput());
+                            mesRecordStaff.setEndPower(iotMachineOutput.getPower());
+                        }
                         mesRecordStaffService.updateById(mesRecordStaff.getId(),mesRecordStaff);
                         //需要后期的一些结算数量操作
                     }
