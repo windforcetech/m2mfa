d0220d95061e491e0118095361da4f2c1517cbd3
chenshuhong <shuhong.chen@m2micro.com>
2019-01-19 10:21:01 +0800

    Pad新增烤料接口

--- src/main/java/com/m2micro/m2mfa/pad/service/PadBakingService.java
+++ src/main/java/com/m2micro/m2mfa/pad/service/PadBakingService.java
@@ -2,8 +2,11 @@ package com.m2micro.m2mfa.pad.service;
 
 import com.m2micro.m2mfa.pad.operate.BaseOperate;
 
+
 /**
- * 烤料service
+ * @Auther: chenshuhong
+ * @Date: 2019/1/19 10:20
+ * @Description:  烤料 服务类
  */
 public interface PadBakingService extends BaseOperate {
 
--- src/main/java/com/m2micro/m2mfa/pad/service/impl/PadBakingServiceImpl.java
+++ src/main/java/com/m2micro/m2mfa/pad/service/impl/PadBakingServiceImpl.java
@@ -2,10 +2,14 @@ package com.m2micro.m2mfa.pad.service.impl;
 
 import com.m2micro.m2mfa.pad.operate.BaseOperateImpl;
 import com.m2micro.m2mfa.pad.service.PadBakingService;
+import org.springframework.stereotype.Service;
 
 /**
- * 烤料serviceImpl
+ * @Auther: chenshuhong
+ * @Date: 2019/1/19 10:20
+ * @Description: 烤料
  */
+@Service("padBakingService")
 public class PadBakingServiceImpl extends BaseOperateImpl  implements PadBakingService {
 
 }
