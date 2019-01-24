52461e582cf48ae340e7ab8bfde4f0ccacdf56cb
chenshuhong <shuhong.chen@m2micro.com>
2019-01-24 15:04:36 +0800

    不良工序

--- src/main/java/com/m2micro/m2mfa/pad/model/Padbad.java
+++ src/main/java/com/m2micro/m2mfa/pad/model/Padbad.java
@@ -8,12 +8,7 @@ import lombok.Data;
 @Data
 @ApiModel(value = "Padbad", description = "Pad不良参数")
 public class Padbad {
-  @ApiModelProperty(value = "上工记录Id")
-  private String rwId;
-  /*@ApiModelProperty(value = "不良编码")
-  private String dctCode;
-  @ApiModelProperty(value = "数量")
-  private Integer qty;*/
+
   @ApiModelProperty(value = "工位Id")
   private String stationId;
   @ApiModelProperty(value = "不良输入")
