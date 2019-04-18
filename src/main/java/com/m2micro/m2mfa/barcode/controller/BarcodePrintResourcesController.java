3f4b135dda70d63ac5616e12eb128b5cd870d687
chenshuhong <shuhong.chen@m2micro.com>
2019-04-18 15:24:32 +0800

    标签码添加修改

--- src/main/java/com/m2micro/m2mfa/barcode/controller/BarcodePrintResourcesController.java
+++ src/main/java/com/m2micro/m2mfa/barcode/controller/BarcodePrintResourcesController.java
@@ -1,5 +1,6 @@
 package com.m2micro.m2mfa.barcode.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.m2micro.framework.authorization.Authorize;
 import com.m2micro.m2mfa.barcode.service.BarcodePrintResourcesService;
 import com.m2micro.framework.commons.exception.MMException;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value="标签打印记录 前端控制器")
 @Authorize
 public class BarcodePrintResourcesController {
+
     @Autowired
     BarcodePrintResourcesService barcodePrintResourcesService;
 
@@ -44,6 +46,7 @@ public class BarcodePrintResourcesController {
         return ResponseMessage.ok(page);
     }
 
+
     /**
      * 详情
      */
@@ -55,6 +58,7 @@ public class BarcodePrintResourcesController {
         return ResponseMessage.ok(barcodePrintResources);
     }
 
+
     /**
      * 保存
      */
@@ -64,8 +68,16 @@ public class BarcodePrintResourcesController {
     public ResponseMessage<BarcodePrintResources> save(@RequestBody BarcodePrintResources barcodePrintResources){
         ValidatorUtil.validateEntity(barcodePrintResources, AddGroup.class);
         barcodePrintResources.setId(UUIDUtil.getUUID());
+        String barcode = barcodePrintResources.getBarcode();
+        JSONObject parse = JSONObject.parseObject(barcode);
+        Object barCode =  parse.get("BarCode");
+        if(barCode !=null){
+            barcodePrintResources.setBarcode((String) barCode);
+        }
         return ResponseMessage.ok(barcodePrintResourcesService.save(barcodePrintResources));
     }
+
+
 
     /**
      * 更新
@@ -83,6 +95,7 @@ public class BarcodePrintResourcesController {
         return ResponseMessage.ok(barcodePrintResourcesService.save(barcodePrintResourcesOld));
     }
 
+
     /**
      * 删除
      */
@@ -93,5 +106,6 @@ public class BarcodePrintResourcesController {
         barcodePrintResourcesService.deleteByIds(ids);
         return ResponseMessage.ok();
     }
+
 
 }
