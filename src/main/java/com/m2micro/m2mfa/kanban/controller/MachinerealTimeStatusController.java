87be9feefc46048651fa3e56be8e33812c1956de
chenshuhong <shuhong.chen@m2micro.com>
2019-06-06 10:49:35 +0800

    看板配置

--- src/main/java/com/m2micro/m2mfa/kanban/controller/MachinerealTimeStatusController.java
+++ src/main/java/com/m2micro/m2mfa/kanban/controller/MachinerealTimeStatusController.java
@@ -4,16 +4,14 @@ import com.m2micro.framework.commons.annotation.UserOperationLog;
 import com.m2micro.framework.commons.model.ResponseMessage;
 import com.m2micro.m2mfa.kanban.service.MachinerealTimeStatusService;
 import com.m2micro.m2mfa.kanban.vo.MachinerealTimeData;
-import com.m2micro.m2mfa.push.MyChatServerHandler;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -27,37 +25,11 @@ public class MachinerealTimeStatusController {
   /**
    * 机台实时数据显示
    */
-  @PostMapping("/MachinerealTimeStatusShow")
+  @PostMapping("/MachinerealTimeStatusShow/{elemen}")
   @ApiOperation(value="机台实时数据显示")
   @UserOperationLog("机台实时数据显示")
-  public ResponseMessage<List<MachinerealTimeData>> MachinerealTimeStatusShow(HttpServletRequest request){
-    System.out.println("ip"+getRemortIP(request));
-    MyChatServerHandler.pushMainScreen(getRemortIP(request));
-    return  ResponseMessage.ok(machinerealTimeStatusService.MachinerealTimeStatusShow(getRemortIP(request)));
-  }
-
-
-  /**
-   * 获取IP地址
-   *
-   * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
-   * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
-   */
-  public String getRemortIP(HttpServletRequest request) {
-
-    String ip = request.getHeader("x-forwarded-for");
-    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-      ip = request.getHeader("Proxy-Client-IP");
-    }
-    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-      ip = request.getHeader("WL-Proxy-Client-IP");
-    }
-    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-      ip = request.getRemoteAddr();
-    }
-    return ip;
-
-
+  public ResponseMessage<List<MachinerealTimeData>> MachinerealTimeStatusShow(@PathVariable("elemen") String elemen){
+    return  ResponseMessage.ok(machinerealTimeStatusService.MachinerealTimeStatusShow(elemen));
   }
 
 
