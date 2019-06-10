357eb9bfe8dccae376897e236986b635cc38e9ef
chenshuhong <shuhong.chen@m2micro.com>
2019-06-10 17:47:44 +0800

    提交

---
+++ src/main/java/com/m2micro/m2mfa/report/repository/MesStaffInfoRepository.java
@@ -0,0 +1,14 @@
+package com.m2micro.m2mfa.report.repository;
+
+import com.m2micro.m2mfa.report.entity.MesStaffInfo;
+import com.m2micro.framework.commons.BaseRepository;
+import org.springframework.stereotype.Repository;
+/**
+ *  Repository 接口
+ * @author chenshuhong
+ * @since 2019-06-10
+ */
+@Repository
+public interface MesStaffInfoRepository extends BaseRepository<MesStaffInfo,String> {
+
+}
